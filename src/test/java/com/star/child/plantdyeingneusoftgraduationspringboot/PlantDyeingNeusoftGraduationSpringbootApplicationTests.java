4e2127e79a3fe8deafe0e41431d1e1c4a47115f0
meng <mmengcx@foxmail.com>
2024-04-20 15:15:15 +0800

    base freamwork architecture

---
+++ src/test/java/com/star/child/plantdyeingneusoftgraduationspringboot/PlantDyeingNeusoftGraduationSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.child.plantdyeingneusoftgraduationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDyeingNeusoftGraduationSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
